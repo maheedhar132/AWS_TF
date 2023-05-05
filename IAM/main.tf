9f61c8015f176e3ca8123f0c0bc1d04fe2b0c8a6
maheedhar132 <maheedhartalluri@gmail.com>
2023-05-05 22:21:30 +0530

    initial commit

---
+++ IAM/main.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_user" "iam_users" {
+    count = length(var.iam_users)
+    name = var.iam_users[count.index]   
+    
+}
+
+resource "aws_iam_policy" "ec2_access" {
+    description = "Provide full access to ec2"
+    name = "Ec2_Full_Access"
+    policy = file("policies/ec2_full_access.json")
+}
+
+resource "aws_iam_policy_attachment" "attach_iam_policy" {
+  count = length(var.iam_users)
+  name = var.iam_users[count.index]
+  policy_arn = aws_iam_policy.ec2_access.arn
+}
